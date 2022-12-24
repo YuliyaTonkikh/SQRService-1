a6aca7343dd5e3e738d7c82291576e6813f4e266
Yuliya Tonkikh <uliya_i@bk.ru>
2022-12-24 21:32:21 +0700

    second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ import ru.netology.SQRService;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        SQRService service = new SQRService(200, 300);
-        System.out.println(service.findNumberOfSquares());
+       System.out.println(SQRService.findNumberOfSquares(200,300));
     }
 }
--- src/test/java/ru/netology/SQRServiceTest.java
+++ src/test/java/ru/netology/SQRServiceTest.java
@@ -6,22 +6,18 @@ class SQRServiceTest {
 
     int left = 0;
     int right = 0;
-    SQRService service = null;
+
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
         this.left = 200;
         this.right = 300;
-        try {
-            this.service = new SQRService(left, right);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+
 
     }
 
     @org.junit.jupiter.api.Test
     void findNumberOfSquares() {
         int expected = 3;
-        assertEquals(expected, this.service.findNumberOfSquares());
+        assertEquals(expected, SQRService.findNumberOfSquares(this.left, this.right));
     }
 }
