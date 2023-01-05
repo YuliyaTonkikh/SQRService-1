8a7a6e6e3c4427d79d74542dec05e50f5980283f
Yuliya Tonkikh <uliya_i@bk.ru>
2023-01-05 12:05:19 +0700

    reformatCode commit

--- src/main/java/ru/netology/SQRService.java
+++ src/main/java/ru/netology/SQRService.java
@@ -3,9 +3,8 @@ package ru.netology;
 public class SQRService {
 
 
-
-       public static int findNumberOfSquares(int left, int right) {
-           int countSquares = 0; // счетчик количества квадратов
+    public static int findNumberOfSquares(int left, int right) {
+        int countSquares = 0; // счетчик количества квадратов
         int i = 0;
         for (i = 10; i <= 99; i++) {
             int value = (int) Math.pow(i, 2);
